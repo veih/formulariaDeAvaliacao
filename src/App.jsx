f2ba31bcf1daa7ec290daf85f0a6df44556aba31
Marcelo evangelista <57013425+veih@users.noreply.github.com>
2022-11-30 20:53:27 -0300

    criação de formulário de avaliação

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos feliz com sua compra, utilize o formulário abaixo para avaliar o produto</p>
+      </div>
+
+      <div className='form-container'>
+        <p>etapa</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className='actions'>
+            <button type='button' className=''>
+              <GrFormPrevious />
+              <span>Voltar</span>
+            </button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default App
